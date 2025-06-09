87960caa6288b35456174649a57acde3ab48db94
Cristina <ccabcar2403@g.educaand.es>
2025-06-09 19:20:24 +0200

    Build

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0', // Escucha todas las interfaces
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: '192.168.18.47', // ← IP de tu servidor en red local
-        },
-    },
+    // server: {
+    //     host: '0.0.0.0', // Escucha todas las interfaces
+    //     port: 5173,
+    //     strictPort: true,
+    //     hmr: {
+    //         //host: '192.168.18.47', // ← IP de tu servidor en red local
+    //         host: '192.168.0.162',
+    //     },
+    // },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
