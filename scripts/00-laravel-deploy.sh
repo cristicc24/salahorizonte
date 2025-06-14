0b1634e9cd153d889a45a511b3a347ba98e01329
Cristina <ccabcar2403@g.educaand.es>
2025-06-14 16:03:19 +0200

    Deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,11 +11,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
-
-echo "Running seeders..."
-php artisan db:seed --force
+echo "Resetting database (fresh migrate with seed)..."
+php artisan migrate:fresh --seed --force
 
 echo "Creating symbolic link..."
 php artisan storage:link
